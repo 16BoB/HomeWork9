bb1df65bfc64a474fcf6b555236bfe95710c3d62
Tape <trid.skivi@gmail.com>
2022-07-21 20:04:05 +0300

    Wrote code for first task

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом.
+// N = 4532 -> 4
+int CountDigitsInNumber(int num)
+{
+    if (num / 10 != 0) return 1 + CountDigitsInNumber(num / 10);
+    else return 1;
+}
+
+Console.WriteLine("Enter the number:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Number of digits in number: " + CountDigitsInNumber(num));
